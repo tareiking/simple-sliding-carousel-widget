502a369c0b6a7873253e77db6e49bc7aa042387a
Tarei King <tareiking@gmail.com>
2014-09-01 13:49:11 +1000

    Add initial slider settings

---
+++ assets/js/slider-main.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function ($) {
+	$('.slick-slider').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 3000,
+		dots: true,
+		arrows: false
+	});
+});
--- templates/slider-template.php
+++ templates/slider-template.php
@@ -12,18 +12,24 @@ $args = wp_parse_args( $args, $defaults );
 $slider_items = new WP_Query( $args );
 
 if ( $slider_items->have_posts() ): ?>
+<div class="slick-widget-area">
+	<div class="slick-slider">
+		<?php while ( $slider_items->have_posts() ) : $slider_items->the_post(); ?>
 
-	<div class="flexslider">
-		<ul class="slides">
-			<?php while ( $slider_items->have_posts() ) : $slider_items->the_post(); ?>
-				<li>
-					<?php if ( has_post_thumbnail() ): ?>
-						<?php the_post_thumbnail( 'slider-thumb' ); ?>
-					<?php endif; ?>
-				</li>
-			<?php endwhile; ?>
-		</ul>
+			<div class="slick-content ">
+
+				<h3><?php echo the_title(); ?></h3>
+
+				<?php if ( has_post_thumbnail() ): ?>
+					<?php the_post_thumbnail( 'slider-thumb' ); ?> <br>
+				<?php endif; ?>
+
+				<a href="#" class="button">Visit Link</a><br>
+
+			</div>
+		<?php endwhile; ?>
 	</div>
+</div>
 <?php else: ?>
 	<p>There aren't any sliders.</p>
 <?php endif; ?>
