946e9016a21dabb8294abfa2b45cf85b28ff7d8d
Tarei King <tareiking@gmail.com>
2014-09-01 10:41:27 +1000

    Remove pico assets

--- js/jquery.slider-main.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function ($) {
-	var options = {
-		controlsContainer: ".slider-navigation",
-		manualControls: ".slider-controls li a"
-	};
-	$('.flexslider').flexslider( options );
-});
